abfe0cadcb2f6a5d462acbb44c7917d9113de596
Aleksey Bulatov <AlexeyABulatov@yandex.ru>
2017-06-30 00:16:48 +0300

    example beginned

---
+++ src/example/example.cpp
@@ -0,0 +1,16 @@
+#include "cic.hpp"
+
+#include <iostream>
+
+using namespace std;
+using namespace cic;
+
+int main(int argc, char** argv)
+{/*
+	Parameters pars(
+			"Allowed options",
+			//ParametersGroup
+			Parameter<bool>()
+	);*/
+	return 0;
+}
