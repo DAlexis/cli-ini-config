153b7f771fc7cc139e2d9de4799e88f64494f771
Aleksey Bulatov <AlexeyABulatov@yandex.ru>
2017-09-26 19:15:54 +0300

    Build system fixes. Todo: collapse build system commits!

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ echo "Usage: ./build.sh <debug|release>. Debug is default"
 
 source setup-dirs.sh
 
-mkdir -p $build_dir
+echo "mkdir -p \"$build_dir\""
+mkdir -p "$build_dir"
+
 (
     cd $build_dir && cmake -DCMAKE_BUILD_TYPE=$cfg $source_dir_from_build_dir && make -j `nproc`
 )
